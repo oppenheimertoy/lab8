b0d9b530e3ee6ca503f19bde03e0d2003c60837e
Ivan Rotankov <titaninus4590@yandex.ru>
2021-12-06 15:41:10 +0300

    working tests

--- test/controllers/calc_controller_test.rb
+++ test/controllers/calc_controller_test.rb
@@ -1,23 +1,24 @@
-require './test/ex'
-class IoControllerTest < ActionDispatch::IntegrationTest
+require_relative '../test_helper'
+class CalcControllerTest < ActionController::TestCase
   test 'should get input' do
-    get io_input_url
+    get :input
     assert_response :success
   end
-  test 'should get view one' do
-    get io_view_url, params: {'arr_len'=>'7', 'arr'=>'6 6 6 5 6 8128 7'}
+  test "should get view one" do
+    get :view, params: {"arr_len"=>"7", "arr"=>"6 6 6 5 6 8128 7"}
     assert_response :success
     count = '2'
     max = [6, 6, 6]
-    assert_select '#count', count
-    assert_select '#max', max.to_s
+    assert_select "#count", count
+    assert_select "#max", max.to_s
   end
-  test 'should get view three' do
-    get io_view_url, params: {'arr_len'=>'4', 'arr'=>'28 6 8128 5 6 8128 7'}
+  
+  test "should get view three" do
+    get :view, params: {"arr_len"=>"4", "arr"=>"28 6 8128 5 6 8128 7"}
     assert_response :success
     count = '1'
     max = [28, 6, 8128]
-    assert_select '#count', count
-    assert_select '#max', max.to_s
+    assert_select "#count", count
+    assert_select "#max", max.to_s
   end
 end
